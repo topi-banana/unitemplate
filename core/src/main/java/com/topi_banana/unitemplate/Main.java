7e12c41369e08a4b76d6365b27ede9760c5f1cac
topi-banana <support@topi-banana.com>
2024-11-02 19:30:01 +0900

    first commit

---
+++ core/src/main/java/com/topi_banana/unitemplate/Main.java
@@ -0,0 +1,7 @@
+package com.topi_banana.unitemplate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ fabric/src/main/java/com/topi_banana/unitemplate/fabric/FabricMain.java
@@ -0,0 +1,33 @@
+package com.topi_banana.unitemplate.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FabricMain implements ModInitializer, ClientModInitializer, DedicatedServerModInitializer {
+
+    public static final String MOD_ID = "unitemplate";
+    public static String MOD_VERSION = "unknown";
+    public static String MOD_NAME = "unknown";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+    }
+
+    @Override
+    public void onInitializeServer() {
+    }
+
+    @Override
+    public void onInitialize() {
+        ModMetadata metadata = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(RuntimeException::new).getMetadata();
+        MOD_NAME = metadata.getName();
+        MOD_VERSION = metadata.getVersion().getFriendlyString();
+        LOGGER.info("hello from unitemplate");
+    }
+}
