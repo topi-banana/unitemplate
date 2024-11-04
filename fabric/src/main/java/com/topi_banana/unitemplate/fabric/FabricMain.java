d7dfcedfe80341116791fee4b7ed617b16b15d8a
topi-banana <support@topi-banana.com>
2024-11-04 11:59:01 +0900

    added paper

--- fabric/src/main/java/com/topi_banana/unitemplate/fabric/FabricMain.java
+++ fabric/src/main/java/com/topi_banana/unitemplate/fabric/FabricMain.java
@@ -1,26 +1,42 @@
 package com.topi_banana.unitemplate.fabric;
 
+import com.topi_banana.unitemplate.core.Unitemplate;
+import com.topi_banana.unitemplate.core.utils.Player;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.metadata.ModMetadata;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FabricMain implements ModInitializer, ClientModInitializer, DedicatedServerModInitializer {
 
     public static final String MOD_ID = "unitemplate";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static String MOD_VERSION = "unknown";
     public static String MOD_NAME = "unknown";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private Unitemplate instance;
 
     @Override
     public void onInitializeClient() {
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {});
     }
 
     @Override
     public void onInitializeServer() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayerEntity player = handler.getPlayer();
+            instance.onJoinPlayer(new Player(player.getUuid(), player.getName().getString()));
+        });
+        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
+            ServerPlayerEntity player = handler.getPlayer();
+            instance.onLeavePlayer(new Player(player.getUuid(), player.getName().getString()));
+        });
     }
 
     @Override
@@ -34,5 +50,7 @@ public class FabricMain implements ModInitializer, ClientModInitializer, Dedicat
         #else
             LOGGER.info("MC_VERSION<12103");
         #endif
+
+        instance = new Unitemplate();
     }
 }
